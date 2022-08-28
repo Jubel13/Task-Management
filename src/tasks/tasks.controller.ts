73d8a5aad688e212799b4deac6a327ce9d76202a
Jubel Sinaga <jubelsinaga13@gmail.com>
2022-08-28 15:16:16 +0700

    Add create task function

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { stringify } from 'querystring';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -9,5 +10,10 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body('title') title, @Body('description') description): Task {
+    return this.tasksService.createTask(title, description);
   }
 }
